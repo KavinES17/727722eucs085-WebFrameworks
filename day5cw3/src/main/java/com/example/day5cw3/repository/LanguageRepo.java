2d76594bd83d1a12cc18e9ed1cd6f4f53f66d516
KavinES17 <151002659+KavinES17@users.noreply.github.com>
2024-04-22 09:07:41 +0530

    Delete day5cw3 directory

--- day5cw3/src/main/java/com/example/day5cw3/repository/LanguageRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.day5cw3.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.day5cw3.model.Language;
-
-public interface LanguageRepo extends JpaRepository<Language,Integer>{
-    
-}
